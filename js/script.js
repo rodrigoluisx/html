110ad1eba761172c0d62a6e1dde856c78b2b03f0
rodrigoluisx <rodrilasek02@gmail.com>
2026-06-25 22:19:42 -0300

    11

--- js/script.js
+++ js/script.js
@@ -7,3 +7,6 @@ function mudar_text(){
 function mudar_cor(){
     window.document.getElementById("titulo-form").style.color="red";
 }
+function mudar_cor2(){
+    window.document.getElementById("titulo-form").style.color="black"
+}
