c2dbcfe9c27d284e73c7df2d4332292b2fa856fa
rodrigoluisx <rodrilasek02@gmail.com>
2026-06-25 21:31:35 -0300

    5

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ window.location.href
 window.alert
 window.confirm("confirma?")
 function mudar_text(){
-    window.document.getElementById("titulo-form")
+    window.document.getElementById("titulo-form").innerText="alterei o titulo"
 }
