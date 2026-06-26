89a79a98722b9f2e45929f37c794e317efb143e9
rodrigoluisx <rodrilasek02@gmail.com>
2026-06-25 21:53:35 -0300

    7

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ window.confirm("confirma?")
 function mudar_text(){
     window.document.getElementById("titulo-form").innerText="alterei o titulo"
 }
+function mudar_cor(){
+    window.document.getElementsbyId("titulo-form").style.color="red"
+}
